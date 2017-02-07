f1f41fac63d9afa9d2a8e205600d5779c53dc34b
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2017-02-07 11:39:11 -0500

    Created basic Character model

--- server/api/character/character.model.js
+++ server/api/character/character.model.js
@@ -3,9 +3,75 @@
 import mongoose from 'mongoose';
 
 var CharacterSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  bio: {
+    name: String,
+    age: Number,
+    gender: String,
+    height: Number,
+    weight: Number,
+    languages: String,
+    backStory: String
+  },
+  race: {
+    primary: String,
+    secondary: String
+  },
+  class: {
+    main: String,
+    path: String
+  },
+  general: {
+    level: Number,
+    experience: Number,
+    alignment: String,
+    baseMove: Number,
+    actualMove: Number,
+  },
+  background: {
+    main: String,
+    moment: Number,
+    trait: Number,
+    ideal: Number,
+    bond: Number,
+    flaw: Number
+  },
+  combat: {
+    armor: String,
+    weapons: String,
+    ammunition: {
+      arrows: Number,
+      bolts: Number,
+      bullets: Number,
+      needles: Number
+    },
+    armorClass: Number,
+    hitPoints: Number,
+    hitDie: String,
+    specialAttacks: String,
+    specialDefense: String
+  },
+  equipment: String,
+  money: {
+    gold: Number,
+    silver: Number,
+    copper: Number
+  },
+  abilityScores: {
+    str: Number,
+    dex: Number,
+    con: Number,
+    int: Number,
+    wis: Number,
+    cha: Number
+  },
+  skills: {
+    prof: String,
+    passive: {
+      insight: Number,
+      perception: Number
+    },
+    profBonus: Number
+  }
 });
 
 export default mongoose.model('Character', CharacterSchema);
