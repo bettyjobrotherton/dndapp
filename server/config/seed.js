a76242705a2632d072e5e10580826608bae1f46f
aleczander <afigueroa81@ufl.edu>
2017-02-09 08:58:20 -0500

    stlying and messing around with text and positioning

--- server/config/seed.js
+++ server/config/seed.js
@@ -10,33 +10,26 @@ import User from '../api/user/user.model';
 Thing.find({}).remove()
   .then(() => {
     Thing.create({
-      name: 'Development Fools',
-      info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
-            + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
-            + 'Stylus, Sass, and Less.',
-      hardware: 35
+      name: 'Characters',
+      info: ''
     }, {
-      name: 'Server and Client integration',
+      name: 'Campaigns',
       info: 'Built with a powerful and fun stack: MongoDB, Express, '
             + 'AngularJS, and Node.'
     }, {
-      name: 'Smart Build System',
+      name: 'Badges',
       info: 'Build system ignores `spec` files, allowing you to keep '
             + 'tests alongside code. Automatic injection of scripts and '
             + 'styles into your index.html'
     }, {
-      name: 'Modular Structure',
+      name: 'Stats',
       info: 'Best practice client and server structures allow for more '
             + 'code reusability and maximum scalability'
     }, {
-      name: 'Optimized Build',
+      name: 'Message Board',
       info: 'Build process packs up your templates as a single JavaScript '
             + 'payload, minifies your scripts/css/images, and rewrites asset '
             + 'names for caching.'
-    }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku '
-            + 'and openshift subgenerators'
     });
   });
 
