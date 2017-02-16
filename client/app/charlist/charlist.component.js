f02bb6e82f0dc91794d672e7913de7c2ef9f4133
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2017-02-16 13:53:00 -0500

    Coded charlist to show basic information for all the user's characters and created page and route for user to view character detail

--- client/app/charlist/charlist.component.js
+++ client/app/charlist/charlist.component.js
@@ -7,22 +7,30 @@ import routes from './charlist.routes';
 
 export class CharListComponent {
 
-  constructor(Auth, $http){
+  constructor(Auth, $http, $state){
     'ngInject';
 
     this.$http = $http;
+    this.$state = $state;
     this.getCurrentUser = Auth.getCurrentUserSync;
-
   }
 
   $onInit(){
       // console.log(this.getCurrentUser());
-      this.getCurrentUser().$promise.then(data => {
+      // this.getCurrentUser().$promise.then(data => {
         // this.user = data;
         // this.characters = data.characters;
-        console.table(data.characters);
-      });
+        // console.table(data.characters);
+      // });
+
   }
+
+  goToCharProfile(data){
+    this.profile = data;
+    this.$state.go('charprofile');
+    console.log(this.profile);
+  }
+
 
 }
 
@@ -30,6 +38,11 @@ export default angular.module('dndappApp.charlist', [uiRouter])
     .config(routes)
     .component('charlist', {
       template: require('./charlist.html'),
+      controller: CharListComponent,
+      controllerAs: 'charCtrl'
+    })
+    .component('charprofile', {
+      template: require('./charprofile.html'),
       controller: CharListComponent,
       controllerAs: 'charCtrl'
     })
