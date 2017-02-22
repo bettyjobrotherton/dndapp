4f1e8e07f48920ff0b5d842daa8d32633974773c
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2017-02-22 14:01:13 -0500

    Started linking data to charprofile using tab viewing; also did basic styling

--- client/app/charlist/charlist.component.js
+++ client/app/charlist/charlist.component.js
@@ -13,21 +13,87 @@ export class CharListComponent {
     this.$http = $http;
     this.$state = $state;
     this.$stateParams = $stateParams;
+    this.character = Character;
     this.getCurrentUser = Auth.getCurrentUserSync;
     this.getProfile = Character.getProfile;
     this.selectChar = Character.returnProfile;
   }
 
   $onInit(){
+    // When user navigates to character profile
     if(this.$state.current.name == 'charprofile2'){
-      this.getProfile(this.$stateParams.id)
-
+      this.getProfile(this.$stateParams.id);
+      this.general = true;
+      this.combat = false;
+      this.skills = false;
+      this.equip = false;
+      this.spells = false;
+      this.misc = false;
+      // this.selectChar().$promise.then(res => {
+      //   this.con = res.abilityScores.con;
+      //   return this.con;
+      // });
+      this.conModifier = this.character.calculateModifier(16);
      }
   }
 
   goToCharProfile(data){
     this.$state.go('charprofile2', {id: data._id});
     console.log(this.$stateParams);
+  }
+
+  showGeneral(){
+    this.general = true;
+    this.combat = false;
+    this.skills = false;
+    this.equip = false;
+    this.spells = false;
+    this.misc = false;
+  }
+
+  showCombat(){
+    this.general = false;
+    this.combat = true;
+    this.skills = false;
+    this.equip = false;
+    this.spells = false;
+    this.misc = false;
+  }
+
+  showSkills(){
+    this.general = false;
+    this.combat = false;
+    this.skills = true;
+    this.equip = false;
+    this.spells = false;
+    this.misc = false;
+  }
+
+  showEquip(){
+    this.general = false;
+    this.combat = false;
+    this.skills = false;
+    this.equip = true;
+    this.spells = false;
+    this.misc = false;
+  }
+
+  showSpells(){
+    this.general = false;
+    this.combat = false;
+    this.skills = false;
+    this.equip = false;
+    this.spells = true;
+    this.misc = false;
+  }
+
+  showMisc(){
+    this.general = false;
+    this.combat = false;
+    this.skills = false;
+    this.equip = false;
+    this.spells = false;
+    this.misc = true;
   }
 
 
