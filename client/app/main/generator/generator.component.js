d96579c2865f03a115c03afeb94988c157e993b5
aleczander <afigueroa81@ufl.edu>
2017-02-28 18:45:06 -0500

    styling the race page in character creator added some text and images

--- client/app/main/generator/generator.component.js
+++ client/app/main/generator/generator.component.js
@@ -11,6 +11,53 @@ export class GeneratorController {
   }
 
   $onInit() {
+    this.raceList = [{
+      name: 'Dragonborn',
+      desc: 'filler',
+      icon: './assets/images/icons/Dragonborn.png'
+    },
+    {
+      name: 'Dwarf',
+      desc: 'filler',
+      icon: './assets/images/icons/Dwarf.png'
+
+    },
+    {
+      name: 'Elf',
+      desc: 'filler',
+      icon: './assets/images/icons/Elf.png'
+    },
+    {
+      name: 'Gnome',
+      desc: 'filler',
+      icon: './assets/images/icons/Gnome.png'
+    },
+    {
+      name: 'Half-Elf',
+      desc: 'filler',
+      icon: './assets/images/icons/Half Elf.png'
+    },
+    {
+      name: 'Half-orc',
+      desc: 'some filler text',
+      icon: './assets/images/icons/Half Orc.png'
+    },
+    {
+      name: 'Halfling',
+      desc: 'filler',
+      icon: './assets/images/icons/Halfling.png'
+    },
+    {
+      name: 'Human',
+      desc: 'filler',
+      icon: './assets/images/icons/Human.png'
+    },
+    {
+      name: 'Tiefling',
+      desc: 'filler',
+      icon: './assets/images/icons/Tiefling.png'
+    },
+  ];
   }
 
   createChar(firstOpt) {
@@ -22,6 +69,9 @@ export class GeneratorController {
     }
   }
 
+  selectRace(race) {
+    this.currentRace = race;
+  }
 }
 
 export default angular.module('dndappApp.generator', [uiRouter])
