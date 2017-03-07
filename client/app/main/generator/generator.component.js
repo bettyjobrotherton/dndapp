ca17f09a6bbe2dce433a3a066a07cf70bc577882
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2017-03-07 12:06:44 -0500

    Created function for submit buttons on class and race to determine whether a new character needs to be created or added to.

--- client/app/main/generator/generator.component.js
+++ client/app/main/generator/generator.component.js
@@ -5,11 +5,12 @@ import routing from './generator.routes';
 export class GeneratorController {
 
   /*@ngInject*/
-  constructor($http, $state, Character) {
+  constructor($http, $state, Character, $window) {
     this.$http = $http;
     this.$state = $state;
     this.createChar = Character.createChar;
     this.first = Character.firstOption;
+    this.localStorage = $window.localStorage;
   }
 
   $onInit() {
@@ -49,9 +50,58 @@ export class GeneratorController {
     this.currentClass = build;
   }
 
-  saveRace(race){
-    console.log(this.first());
+  saveRace(){
+    var newCharacter;
+    var race = this.currentRace;
+    var raceInfo = {
+      bio: {
+        languages: race.traits.lang,
+        appearance: {
+          size: race.traits.size
+        }
+      },
+      general: {
+        movement: race.traits.baseSpd
+      },
+      race: {
+        main: race.name,
+        //subrace: **This needs to be added to the JSON file**
+      }
+    };
+    if(this.first() == 'race'){
+      newCharacter = raceInfo;
+      this.localStorage['character-in-progress'] = newCharacter;
+    } else {
+      newCharacter = this.localStorage['character-in-progress'];
+      newCharacter.push(raceInfo);
+      this.localStorage['character-in-progress'] = newCharacter;
+    }
+  }
 
+  saveClass(){
+    // console.log(this.first());
+    var newCharacter;
+    var currentClass = this.currentClass;
+    var classInfo = {
+      //This is all greyed out because most of the needed info is not yet in the JSON file
+      // class: {
+      //   main: currentClass.name,
+      //   archetype:
+      // },
+      // combt: {
+      //   hitDie:
+      //   specialAttacks:
+      //   specialDefense:
+      // }
+    };
+    if(this.first() == 'class'){
+      newCharacter = classInfo;
+      this.localStorage['character-in-progress'] = newCharacter;
+    } else {
+      newCharacter = this.localStorage['character-in-progress'];
+      newCharacter.push(classInfo);
+      this.localStorage['character-in-progress'] = newCharacter;
+    }
   }
 }
 
